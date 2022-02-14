c8e3364ad14657284b98a988a935c11b0195efe9
Hanxiang Li <itsupport7@opulence.com>
2022-02-14 12:31:45 -0500

    use axois connect front and back end

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,53 +2,28 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { io } from "socket.io-client";
-import { useEffect,useState } from 'react';
+import React, { useEffect,useState } from 'react';
+import { stringifyQuery } from 'next/dist/server/server-route-utils';
 
 export default function Home() {
 
-  const socket = io("http://localhost:5000/",{
-    withCredentials: true,
-    extraHeaders: {
-      "my-custom-header": "abcd"
-    }
-  });  
+  // const [socketState, setSocketState] = useState(null);
+  const socket = io("http://localhost:5000/");
+
+  // useEffect(()=>{
+    socket.on('geoData', (data) => {console.log('socketState->',JSON.stringify(data))});
+    // socket.on('geoData', (data) => {setSocketState(data)});
+    // if(socketState != null){console.log('socketState->',socketState)}
+  // })
   
-
-// useEffect(()=>{
-//   let socket = io.connect("http://localhost:5000/");
-//   socket.on('userCount', function(data){
-//       console.log(data);
-//       document.getElementById("getcount").innerHTML = "currenty " + data.userCount + " user connected";
-//   })
-// })
-// const [socket, setSocket] = useState(null);
-// useEffect(() => {
-//   const newSocket = io(`http://localhost:5000/`);
-//   setSocket(newSocket);
-//   return () => newSocket.close();
-// }, [setSocket]);
-
-
-
-// client-side
-socket.on("connect", () => {
-  console.log("ssssuccesssed!!!"); // x8WIv7-mJelg7on_ALbx
-});
-
   return (
       <div style={{height: "100vh", display: "flex", justifyContent:"center", alignItems: "center"}}>
-        <div>client content</div>
+        <div>client content=</div>
           <div id="getcount">
-            <script src="/socket.io/socket.io.js"></script>
-            <div>{socket ? (
-                     <div className="chat-container">
-                     <div socket={socket} />
-                     <div socket={socket} />
-                   </div>
-                 ) : (
-                   <div>Not Connected</div>
-                 )}
-            </div>
+            <script src="/socket.io/socket.io.js"></script>   
+            <div>
+              {/* {socketState ? socketState : "nothing found"}   */}
+            </div>         
           </div>
         </div>
   )
--- server/index.js
+++ server/index.js
@@ -4,23 +4,48 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const axios = require('axios');
+const querystring = require('querystring');
+
+
 app.get('/', (req, res) => {      
     res.sendFile(__dirname + '/index.html');
 });
 
+
+
+
+
+
+
+
 let userCount = 0;
 io.on('connection', function (socket){
-
+    // if a new window open, count as a new user
+    console.log('connecting');  
     userCount++;
     io.emit('userCount', {userCount: userCount});
     
+    
+    // start
+        const url = 'http://192.168.75.204:8001/api/metrics/get_login_info';
+        let res = axios.post(url).then((res)=>{console.log('res->',res);
+        });
+    // end
+
+
+    socket.emit('geoData',{
+        // geoData: 'Hello there'
+        geoData: res
+    });
+
+    // if user disconnect, count minus 1
     socket.on('disconnect', function () {
         userCount--;
         io.emit('userCount', {userCount: userCount});
         console.log('disconnect');      
     })
 })
-
 
 server.listen(5000, () => {
   console.log('listening on *:5000');
